dd2d7806b7c034993652b2528210a4d57c8b58d4
ika.grabon <ika.grabon@gmail.com>
2017-11-13 20:14:44 +0100

    Add offset

--- tetris.js
+++ tetris.js
@@ -13,15 +13,15 @@ const matrix = [
     [0, 1, 0]
 ];
 
-function drawMatrix(matrix) {
+function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if (value !== 0) {
                 context.fillStyle = "red";
-                context.fillRect(x, y, 1, 1);
+                context.fillRect(x + offset.x, y + offset.y, 1, 1);
             }
         });
     });
 }
 
-drawMatrix(matrix);
+drawMatrix(matrix, {x:5, y:5});
