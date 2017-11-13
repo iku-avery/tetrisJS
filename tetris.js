1d5df04a58fb8af5a50ac88a9d2ed3aa4d6341ea
ika.grabon <ika.grabon@gmail.com>
2017-11-13 20:07:44 +0100

    Add first test-piece tetris in js Change scale canvas

--- tetris.js
+++ tetris.js
@@ -3,5 +3,22 @@ const context = canvas.getContext('2d');
 var W = canvas.width;
 var H = canvas.height;
 
+context.scale(20, 20);
 context.fillStyle = "#000";
 context.fillRect(0, 0, W, H);
+
+const matrix = [
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 1, 0]
+];
+
+matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+        if (value !== 0) {
+        context.fillStyle = "red";
+        context.fillRect(x, y, 1, 1);
+        }
+    });
+});
+
