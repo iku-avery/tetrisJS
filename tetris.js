226e5a0eb0af098b83eb23af56ca92c26704c4f4
ika.grabon <ika.grabon@gmail.com>
2017-11-13 18:57:34 +0100

    Add test fill

--- tetris.js
+++ tetris.js
@@ -1,2 +1,7 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
+var W = canvas.width;
+var H = canvas.height;
+
+context.fillStyle = "#000";
+context.fillRect(0, 0, W, H);
